bfbeb6b11c6bb4b73d20251990acefc4e1c69f95
hongocthaovy0710 <hongocthaovy66@gmail.com>
2025-05-01 15:28:36 +0700

    new

---
+++ app/src/main/java/com/example/admindacs3/OutForDeliveryActivity.kt
@@ -0,0 +1,15 @@
+package com.example.admindacs3
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class OutForDeliveryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_out_for_delivery)
+
+    }
+}
