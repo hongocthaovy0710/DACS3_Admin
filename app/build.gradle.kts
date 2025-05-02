6bf97e4b110d34d09f337be1ddfa7a479cf85f71
hongocthaovy0710 <hongocthaovy66@gmail.com>
2025-05-02 15:46:24 +0700

    Vy da duoc giai thoat

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -41,11 +42,18 @@ android {
 
 dependencies {
 
+
+    implementation(libs.firebase.auth.ktx)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.auth)
+    implementation(libs.androidx.credentials)
+    implementation(libs.androidx.credentials.play.services.auth)
+    implementation(libs.googleid)
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/admindacs3/SignUpActivity.kt
+++ app/src/main/java/com/example/admindacs3/SignUpActivity.kt
@@ -3,23 +3,58 @@ package com.example.admindacs3
 import android.R
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.admindacs3.databinding.ActivitySignUpBinding
+import com.example.admindacs3.model.UserModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.DatabaseReference
+
+
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var email : String
+    private lateinit var password : String
+    private lateinit var userName : String
+    private lateinit var nameOfRestaurant : String
+    private lateinit var database : DatabaseReference
+
     private val binding: ActivitySignUpBinding by lazy {
         ActivitySignUpBinding.inflate(layoutInflater)
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        binding.CreateButton.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+        //auth
+        auth = FirebaseAuth.getInstance()
+        //database
+        database = Firebase.database.reference
+
+
+        binding.createUserButton.setOnClickListener {
+
+            userName = binding.name.text.toString().trim()
+            nameOfRestaurant = binding.restaurantName.text.toString().trim()
+            email = binding.emailOrPhone.text.toString().trim()
+            password = binding.passsword.text.toString().trim()
+
+            if (userName.isBlank() || nameOfRestaurant.isBlank() || email.isBlank() || password.isBlank())  {
+                Toast.makeText(this,"Please fill all fields", Toast.LENGTH_SHORT).show()
+            }else{
+                createAccount(email,password)
+            }
+
+
 
         }
         binding.alreadyHaveAccountButton.setOnClickListener {
@@ -33,5 +68,36 @@ class SignUpActivity : AppCompatActivity() {
         val adapter = ArrayAdapter(this, R.layout.simple_list_item_1, locationList)
         val autoCompleteTextView = binding.listOfLocation
         autoCompleteTextView.setAdapter(adapter)
+    }
+
+    private fun createAccount(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Toast.makeText(this, "Create account successfully", Toast.LENGTH_SHORT).show()
+                saveUserData()
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+
+            }
+            else {
+                Toast.makeText(this, "Create account failed", Toast.LENGTH_SHORT).show()
+                Log.d("Account", "createAccount: Failure", task.exception)
+            }
+        }
+    }
+// save data in to database
+    private fun saveUserData() {
+        //get text from edittext
+        userName = binding.name.text.toString().trim()
+        nameOfRestaurant = binding.restaurantName.text.toString().trim()
+        email = binding.emailOrPhone.text.toString().trim()
+        password = binding.passsword.text.toString().trim()
+        val user = UserModel(userName, nameOfRestaurant, email, password)
+        val userId: String = FirebaseAuth.getInstance().currentUser!!.uid
+    //save user data firebase database
+        database.child("user").child(userId).setValue(user)
+
+
     }
 }
