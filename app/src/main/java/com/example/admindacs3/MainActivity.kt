79c6649f170b4aaedc9c55db37bfd2ca02e7d14f
hongocthaovy0710 <hongocthaovy66@gmail.com>
2025-05-12 19:50:20 +0700

    xong33

--- app/src/main/java/com/example/admindacs3/MainActivity.kt
+++ app/src/main/java/com/example/admindacs3/MainActivity.kt
@@ -4,14 +4,26 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.admindacs3.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class MainActivity : AppCompatActivity() {
     private val binding : ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
+    private lateinit var database: FirebaseDatabase
+    private lateinit var auth: FirebaseAuth
+    private lateinit var completedOrderReference: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
         binding.addMenu.setOnClickListener {
             val intent = Intent(this, AddItemActivity::class.java)
             startActivity(intent)
@@ -41,5 +53,24 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this,PendingOrderActivity ::class.java)
             startActivity(intent)
         }
+        pendingOrders()
+    }
+
+    private fun pendingOrders() {
+        database = FirebaseDatabase.getInstance()
+        var pendingOrderReference = database.reference.child("OrderDetails")
+        var pendingOrderItemCount = 0
+        pendingOrderReference.addListenerForSingleValueEvent(object :ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                pendingOrderItemCount = snapshot.childrenCount.toInt()
+                binding.pendingOrders.text = pendingOrderItemCount.toString()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
+
     }
 }
